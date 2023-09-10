6651beb19500d87f561628ba6878fb376021233d
ashutoshdhaked <109585979+ashutoshdhaked@users.noreply.github.com>
2023-09-10 15:40:49 +0530

    Add files via upload

---
+++ VideoConference/src/test/java/com/videoconference/VideoConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videoconference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoConferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
